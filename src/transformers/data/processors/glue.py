7e852332f3bac77ee5b62bb0c0472d8e5d69293c
Riya Mokashi <41878830+RiyaMokashi@users.noreply.github.com>
2020-03-16 02:23:13 -0400

    test 7

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -529,10 +529,10 @@ class BoolQProcessor(DataProcessor):
 
     def get_train_examples(self, data_dir):
         logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.jsonl")))
-        return self._create_examples(self.jsonlines.open(os.path.join(data_dir, "train.jsonl")), "train")
+        return self._create_examples(jsonlines.open(os.path.join(data_dir, "train.jsonl")), "train")
 
     def get_dev_examples(self, data_dir):
-        return self._create_examples(self.jsonlines.open(os.path.join(data_dir, "val.jsonl")), "dev")
+        return self._create_examples(jsonlines.open(os.path.join(data_dir, "val.jsonl")), "dev")
 
     def get_labels(self):
         return ["0", "1"]
